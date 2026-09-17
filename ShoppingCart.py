60b9f620b807a5f1c2807f0917662507746a9fc7
Noah Gutierrez <nag23028@uconn.edu>
2026-09-17 12:40:26 -0400

    Implement ShoppingCart class with basic functionality

---
+++ ShoppingCart.py
@@ -0,0 +1,23 @@
+class ShoppingCart:
+
+  def __init__(self) -> None:
+    self.items: list = []
+
+  def add_product(self, product) -> None:
+    self.items.append(product)
+
+  def remove_product(self, product_id: str) -> bool:
+    for index, item in enumerate(self.items):
+      if item.get_id() == product_id:
+        self.items.pop(index)
+        return True
+    return False
+
+  def get_items(self) -> list:
+    return self.items
+
+  def calculate_total(self) -> float:
+    return float(sum(item.get_price() for item in self.items))
+
+  def is_empty(self) -> bool:
+    return len(self.items) == 0
